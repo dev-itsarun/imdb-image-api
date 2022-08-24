cf4d5f52f967971a64547e4e0b5a4af178aaed15
Arun Kumar <109509109+dev-itsarun@users.noreply.github.com>
2022-08-24 18:42:17 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,31 @@
 <?php
 
-//error_reporting(0);
-if(extension_loaded('gd')){echo "loaded";}
+error_reporting(0);
+
+include "simple_html_dom.php";
+
+$file = $_GET["file"];
+
+if (!empty($file)) {
+
+    $html = file_get_html($file);
+
+    $items = $html->find('meta[property=og:image]');
+
+    $src =  $items[0]->content;
+
+    $final_source = str_replace("._V1_FMjpg_UX1000_", "", $src);
+
+    // basic headers
+    header("Content-type: image/jpeg");
+    
+    // get the file name
+    $file= file_get_contents($final_source);
+    
+    echo $file;
+
+
+} 
+else {
+    http_response_code(400);
+}
