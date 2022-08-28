6c2437602e54027605cb228bf9df004f99f16a4c
Arun Kumar <109509109+dev-itzarun@users.noreply.github.com>
2022-08-28 08:06:38 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,30 +2,4 @@
 
 error_reporting(0);
 
-include "simple_html_dom.php";
-
-$file = $_GET["file"];
-
-if (!empty($file)) {
-
-    $html = file_get_html($file);
-
-    $items = $html->find('meta[property=og:image]');
-
-    $src =  $items[0]->content;
-
-    $final_source = str_replace("._V1_FMjpg_UX1000_", "", $src);
-
-    // basic headers
-    header("Content-type: image/jpeg");
-    
-    // get the file name
-    $file= file_get_contents($final_source);
-    
-    echo $file;
-
-
-} 
-else {
-    http_response_code(400);
-}
+echo $_SERVER['testenv'];
